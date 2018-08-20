9215526cfd05dbd29ae692969061a47fa88de800
philippe-ancsin <37195112+philippe-ancsin@users.noreply.github.com>
2018-08-20 18:44:18 +0200

    Update packages.bash

--- packages.bash
+++ packages.bash
@@ -6,34 +6,39 @@ set -e
 
 # deb
 sudo apt install \
-	vim \
-	make \
-	cmake \
-	build-essential \
-	gdb \
-	git \
-	python python3 \
-	python-pip python3-pip
+        vim \
+        make \
+        cmake \
+        build-essential \
+        gdb \
+        git \
+        python python3 \
+        python-pip python3-pip
 
 # vundle (package manager) for vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # get .vimrc from repository
 user="philippe-ancsin"
 repository="deb-bootstrap"
 branch="master"
 raw_file=".vimrc"
-tmp_path="/tmp/"$raw_file
-dest_path="~/.vimrc"
-curl -o $tmp_path https://raw.githubusercontent.com/$user/$repository/$branch/$raw_file
-cp $get_path $dest_path
+tmp_path="/tmp/${raw_file}"
+dest_path="${HOME}/.vimrc"
+echo "get .vimrc ..."
+curl -o $tmp_path "https://raw.githubusercontent.com/${user}/${repository}/${branch}/${raw_file}"
+echo "install ..."
+echo "${tmp_path}->${dest_path}"
+cp $tmp_path $dest_path
 
+echo "install plugins ..."
 vim +PluginInstall +qall
 
 # nvm (node version manager)
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- "https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh" | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 # node
+echo "install node ..."
 nvm install node
